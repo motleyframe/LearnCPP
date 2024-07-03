89a6326154f56fdf98475bd6317588c68f4665ba
Boris Piaterikov <krutotros@gmail.com>
2024-07-03 13:09:37 -0400

    IDF search improved

--- 01/12-Search_Server_IDF_TF.cpp
+++ 01/12-Search_Server_IDF_TF.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <set>
-#include <map>
 #include <algorithm>
 #include <cmath>
+#include <iostream>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
 
 using namespace std::string_literals;
 const int MAX_RESULT_DOCUMENT_COUNT = 5;
@@ -69,7 +69,6 @@ private:
         return words;
     }
 
-
     Query ParseQuery(const std::string& text) const {
         Query query;
         for(const std::string& w : SplitIntoWordsNoStop(text)) {
@@ -83,27 +82,41 @@ private:
         return query;
     }
 
-    std::vector<Document> FindAllDocuments(const Query& query) const {
-        std::map<int, double> tf_idf;
-        for(const auto& p_word : query.plus_words) {
-            if(!word_documents_freq_.count(p_word))
-                continue;
-
-            const auto& freq = word_documents_freq_.at(p_word);
-            double idf = freq.size() == static_cast<size_t>(document_count_)
+    double CalcWordIDF(const std::string& word) const {
+            const auto& freq = word_documents_freq_.at(word);
+            return freq.size() == static_cast<size_t>(document_count_) || freq.size() == 0
                        ? 0.0
                        : log(document_count_ / static_cast<double>(freq.size()));
+    }
 
-            for(const auto& [id, _] : freq)
-                tf_idf[id] += freq.at(id) * idf;
-        }
-
-        for(const std::string& w : query.minus_words) {
-            if(!word_documents_freq_.count(w))
+    // Получить ID документов, в которых есть минус-слова
+    std::set<int> GetInvalidIDs(const Query& query) const {
+        std::set<int> invalid_ids;
+        for(const std::string& m : query.minus_words) {
+            if(!word_documents_freq_.count(m))
                 continue;
 
-            for(const auto &[doc_id, _] : word_documents_freq_.at(w))
-                tf_idf.erase(doc_id);
+            for(const auto &[bad_id, _] : word_documents_freq_.at(m))
+                invalid_ids.insert(bad_id);
+        }
+        return invalid_ids;
+    }
+
+    std::vector<Document> FindAllDocuments(const Query& query) const {
+        std::map<int, double> tf_idf;
+        const std::set<int> invalid_ids = GetInvalidIDs(query);
+
+        for(const auto& p : query.plus_words) {
+            if(!word_documents_freq_.count(p))
+                continue;
+
+            double idf = CalcWordIDF(p);
+            const auto& freq = word_documents_freq_.at(p);
+
+            for(const auto& [id, _] : freq)
+                //только документы, где нет минус-слов
+                if(!invalid_ids.count(id))
+                    tf_idf[id] += idf * freq.at(id);
         }
 
         std::vector<Document> matched_documents;
@@ -122,8 +135,10 @@ public:
 
     void AddDocument(int doc_id, const std::string& document) {
         const std::vector<std::string> words = SplitIntoWordsNoStop(document);
+        double offset = 1.0 / words.size();
+
         for(const std::string& w : words) {
-            word_documents_freq_[w][doc_id] += 1.0 / words.size();
+            word_documents_freq_[w][doc_id] += offset;
         }
         ++document_count_;
     }
