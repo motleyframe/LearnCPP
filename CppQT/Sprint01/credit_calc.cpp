ee03f5de5d57f9d1aad40cd2f8dfdcce128d7761
Boris Piaterikov <realloc@yahoo.com>
2025-01-30 11:09:10 -0500

    ...

--- CppQT/Sprint01/credit_calc.cpp
+++ CppQT/Sprint01/credit_calc.cpp
@@ -1,122 +1,32 @@
 #include <iostream>
-#include <set>
-#include <string>
-#include <algorithm>
-#include <vector>
+#include <cmath>
+using namespace std::string_literals;
 
-using namespace std;
-
-std::string ReadLine() {
-    std::string s;
-    std::getline(std::cin, s);
-    return s;
-}
-
-int ReadLineWithNumber() {
-    int n = 0;
-    std::cin >> n;
-    ReadLine();
-    return n;
-}
-
-std::vector<std::string> SplitIntoWords(const std::string& text) {
-    std::vector<std::string> words;
-    std::string word;
-    for (const char c : text) {
-        if (c == ' ') {
-            if (!word.empty()) {
-                words.push_back(word);
-                word.clear();
-            }
-        } else word += c;
-    }
-    if (!word.empty())
-        words.push_back(word);
-
-    return words;
-}
-
-std::set<std::string> ParseStopWords(const std::string& text) {
-    std::set<std::string> stop_words;
-    for (const std::string& word : SplitIntoWords(text)) {
-        stop_words.insert(word);
-    }
-    return stop_words;
-}
-
-std::vector<std::string> SplitIntoWordsNoStop(const std::string& text, const std::set<std::string>& stop_words) {
-    std::vector<std::string> words;
-    for (const std::string& word : SplitIntoWords(text)) {
-        if (!stop_words.count(word))
-            words.push_back(word);
-    }
-    return words;
-}
-
-void AddDocument(std::vector<std::pair<int,std::vector<std::string>>>& Docs,
-                 const std::set<std::string>& stop_words,
-                 int& doc_id,
-                 const std::string& document) {
-    const std::vector<std::string> words = SplitIntoWordsNoStop(document, stop_words);
-    Docs.push_back( {doc_id,words} );
-}
-
-std::set<std::string> ParseQuery(const std::string& text,
-                                 const std::set<std::string>& stop_words) {
-    set<string> query_words;
-    for (const std::string& word : SplitIntoWordsNoStop(text, stop_words)) {
-        query_words.insert(word);
-    }
-    return query_words;
-}
-
-int MatchDocument(const std::pair<int,std::vector<std::string>>& document_words,
-                  const std::set<std::string>& query_words) {
-    int rel=0;
-    std::vector<std::string> buf = document_words.second;
-
-    for(const auto& b : buf) {
-        if(query_words.count(b))
-            ++rel;
-    }
-
-    return rel;
-}
-
-// Для каждого найденного документа возвращает его {rel,id}
-std::vector<std::pair<int,int>> FindDocuments(const std::vector<std::pair<int,vector<string>>>& Docs,
-                                              const std::set<std::string>& stop_words,
-                                              const std::string& query) {
-    const std::set<std::string> query_words = ParseQuery(query, stop_words);
-
-    std::vector<std::pair<int,int>> matched_documents;
-    int rel=0;
-
-    for (const auto& d : Docs) {
-        rel=MatchDocument(d, query_words);
-        if(rel) {
-            matched_documents.push_back({d.first,rel});
-        }
-    }
-    return matched_documents;
+void Print(const int& month, const double& payment,
+           const double& interest, const double& debt) {
+    std::cout <<"Месяц: "s<<month<<
+                " Платёж: "s<<payment<<
+                " Проценты: "s<<interest<<
+                " Долг: "s<<debt<<std::endl;
 }
 
 int main() {
-    const std::string stop_words_joined = ReadLine();
-    const std::set<std::string> stop_words = ParseStopWords(stop_words_joined);
+    int s,t;
+    double k;
+    std::cin>>s>>k>>t;
 
-    // Read documents
-    std::vector<std::pair<int,vector<string>>> DB;
-    const int N = ReadLineWithNumber();
-    for (int doc_id = 0; doc_id < N; ++doc_id) {
-        AddDocument(DB, stop_words, doc_id, ReadLine());
+    const double m = k/1200;
+    const double power = std::pow((1+m),t);
+    const double a = m*power / (power-1);
+    const double x = s*a;
+    double debt = s;
+
+    for(int i=1; i<=t; ++i) {
+        const double p=debt*m;
+        const double f=x-p;
+        Print(i,x,p,f);
+        debt -= f;
     }
 
-    const std::string query = ReadLine();
-
-    for (auto& docs_found : FindDocuments(DB, stop_words, query))
-        std::cout << "{ document_id = "s << docs_found.first
-                  << ", relevance = "s << docs_found.second <<" }\n"s ;
-
-    return EXIT_SUCCESS;
+    return 0;
 }
